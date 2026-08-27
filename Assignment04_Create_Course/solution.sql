1527712f98df2d3c897ec1fdc2986e6c3d97dec4
naveenkumarkumar60104-cmyk <naveenkumarkumar60104@gmail.com>
2026-08-27 11:39:13 +0530

    Update solution.sql

--- Assignment04_Create_Course/solution.sql
+++ Assignment04_Create_Course/solution.sql
@@ -2,8 +2,12 @@ DROP DATABASE IF EXISTS CollegeDB;
 CREATE DATABASE CollegeDB;
 USE CollegeDB;
 
--- Create Course table
-
--- Insert three records
-
--- Display structure
+CREATE TABLE Course (
+    CourseID NUMBER(5) PRIMARY KEY,
+    CourseName VARCHAR(30) NOT NULL,
+    Credits NUMBER(2),
+    DepartmentID NUMBER(5),
+    CONSTRAINT FK_Course_Department
+    FOREIGN KEY (DepartmentID)
+    REFERENCES Department(DepartmentID)
+);
